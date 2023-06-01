92a8ea4f50aeb243483c6288f22af68affb58b27
Piyush <celestious.hawk@gmail.com>
2023-06-01 13:41:32 +0530

    feat: Create all and single GET routes for rockets

--- backend/index.js
+++ backend/index.js
@@ -4,25 +4,25 @@ import fetch from "node-fetch";
 const app = express();
 
 app.get("/rockets", async (req, res) => {
-  const url = `https://api.spacexdata.com/v3/rockets`;
-  try {
-    const response = await fetch(url);
-    const json = await response.json();
-    res.send(json);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-app.get("/rockets/:id", async (req, res) => {
-  const url = `https://api.spacexdata.com/v3/rockets/${req.params.id}`;
-  try {
-    const response = await fetch(url);
-    const json = await response.json();
-    res.send(json);
-  } catch (err) {
-    console.log(err);
-  }
-});
+    const url = `https://api.spacexdata.com/v3/rockets`;
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      res.send(json);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  
+  app.get("/rockets/:id", async (req, res) => {
+    const url = `https://api.spacexdata.com/v3/rockets/${req.params.id}`;
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      res.send(json);
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
 app.listen(3000, () => console.log("server is running at post 3000"));
