467d15d80a23e7d85f023eef18ed170e74c4ebd0
Piyush <celestious.hawk@gmail.com>
2023-06-02 11:39:19 +0530

    feat: Create Footer component for the landing page

---
+++ front-end/react-spacex/src/components/footer/Footer.jsx
@@ -0,0 +1,28 @@
+import Logo from '../../assets/spacex-logo-white.png';
+import './Footer.css';
+
+const Footer = () => {
+    return(
+        <>
+			<footer>
+				<div className="company">
+					<div className="company-logo">
+						<img src={Logo} alt="" />
+					</div>
+					<div className="company-address">
+						<p>
+							1 Rocket Rd, Hawthorne, CA 90250, United States
+						</p>
+					</div>
+				</div>
+				<div className="copyright">
+					<p>
+						&#169; SpaceX. This is a project serving data through SpaceX API.
+					</p>
+				</div>
+			</footer>
+		</>
+    )
+}
+
+export default Footer;
