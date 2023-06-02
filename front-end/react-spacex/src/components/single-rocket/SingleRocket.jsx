f9589e6bd2312de70bd9fc6b01193e7420ca4372
Piyush <celestious.hawk@gmail.com>
2023-06-02 22:43:21 +0530

    feat: Populate data-fields with data from props

--- front-end/react-spacex/src/components/single-rocket/SingleRocket.jsx
+++ front-end/react-spacex/src/components/single-rocket/SingleRocket.jsx
@@ -1,78 +1,85 @@
 import RocketTestImage from '../../assets/test.jpg'
 import './SingleRocket.css';
 
-const SingleRocket = () => {
+const SingleRocket = ({ showRocketModal, singleRocketData }) => {
 	return(
 		<>
 			<section className="single-rocket-wrapper-bg">
 				<div className="single-rocket-wrapper">
 					<div className="single-rocket-close">
-						<i className="fa-solid fa-xmark"></i>
+						<i className="fa-solid fa-xmark" onClick={() => showRocketModal(false)}></i>
 					</div>
 					<div className="single-rocket-images">
-						<img src={RocketTestImage} alt="" />
+						<img src={singleRocketData.flickr_images[1]} onError={(e) => e.target.src=RocketTestImage} alt="Representational Image of Rocket" />
 					</div>
 					<div className="single-rocket-stats">
 						<div className="single-rocket-name">
-							Falcon Heavy
-							<i></i>
+							{singleRocketData.name}
 						</div>
 						<div className="single-rocket-summary">
 							<div className="rocket-engine rocket-stat">
-								<span className="stat-heading">Engines</span>
-								<span className="stat-value">9</span>
+								<span className="stat-heading">
+									Engines
+								</span>
+								<span className="stat-value">
+									{singleRocketData.engines.number}
+								</span>
 							</div>
 							<div className="separator"></div>
 							<div className="rocket-stages rocket-stat">
 								<span className="stat-heading">Stages</span>
-								<span className="stat-value">3</span>
+								<span className="stat-value">
+									{singleRocketData.stages}
+								</span>
 							</div>
 							<div className="separator"></div>
 							<div className="rocket-boosters rocket-stat">
 								<span className="stat-heading">Boosters</span>
-								<span className="stat-value">2</span>
+								<span className="stat-value">
+									{singleRocketData.boosters}
+								</span>
 							</div>
 						</div>
 						<div className="single-rocket-description">
 							<p>
-								With the ability to lift into orbit over 54 metric tons (119,000 lb)--a mass equivalent to a 737 jetliner loaded with passengers, crew, luggage and fuel--Falcon Heavy can lift more than twice the payload of the next closest operational vehicle, the Delta IV Heavy, at one-third the cost.
+								{singleRocketData.description}
 							</p>
 						</div>
 						<div className="single-rocket-specs">
 							<div className="single-rocket-height rocket-spec">
 								<i className="fa-solid fa-ruler-vertical"></i>
 								<span>
-									Height: 70 meters
+									Height: {singleRocketData.height.meters} meters
 								</span>
 							</div>
 							<div className="single-rocket-height rocket-spec">
 								<i className="fa-solid fa-dollar-sign"></i>
 								<span>
-									Cost: 700,00,000
+									Cost: {singleRocketData.cost_per_launch.toLocaleString()}
 								</span>
 							</div>
 							<div className="single-rocket-diameter rocket-spec">
 								<i className="fa-solid fa-ruler-horizontal"></i>
 								<span>
-									Diameter: 3.7 meters
+									Diameter: {singleRocketData.diameter.meters} meters
 								</span>
 							</div>
 							<div className="single-rocket-mass rocket-spec">
 								<i className="fa-solid fa-weight-hanging"></i>
 								<span>
-									Mass: 54,098 kg
+									Mass: {singleRocketData.mass.kg.toLocaleString()} kg
 								</span>
 							</div>
 							<div className="single-rocket-legs rocket-spec">
 								<i className="fa-solid fa-rocket"></i>
 								<span>
-									Landing Legs: 3
+									Landing Legs: {singleRocketData.landing_legs.number}
 								</span>
 							</div>
 							<div className="single-rocket-success-rate rocket-spec">
 								<i className="fa-regular fa-circle-check"></i>
 								<span>
-									Success Rate: 91%
+									Success Rate: {singleRocketData.success_rate_pct}%
 								</span>
 							</div>
 						</div>
