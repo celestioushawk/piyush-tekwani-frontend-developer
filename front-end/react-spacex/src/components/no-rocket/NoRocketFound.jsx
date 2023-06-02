3ceef2b311dd31f5cd0e06a48a8584a6823020d7
Piyush <celestious.hawk@gmail.com>
2023-06-02 22:36:12 +0530

    feat: Create NoRocketFound component to show as fallback

---
+++ front-end/react-spacex/src/components/no-rocket/NoRocketFound.jsx
@@ -0,0 +1,16 @@
+import FallbackRocketImage from "../../assets/rocket.png";
+import "./NoRocketFound.css";
+const NoRocketFound = () => {
+	return(
+		<>
+			<div className="fallback-message">
+				<img src={FallbackRocketImage} alt="Fallback Image for No Rocket Found" />
+				<h4>
+					Sorry, no matching rockets found!
+				</h4>
+			</div>
+		</>
+	)
+}
+
+export default NoRocketFound;
